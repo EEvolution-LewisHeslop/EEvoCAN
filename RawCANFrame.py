0a83d87057f7c5c140152712d790079a97f35a98
EEvolution-LewisHeslop <lewis.heslop@eevbsl.co.uk>
2023-11-30 11:24:38 +0000

    qol changes in raw can frame

--- RawCANFrame.py
+++ RawCANFrame.py
@@ -19,6 +19,11 @@ class RawCANFrame(customtkinter.CTkFrame):
         self.rawCANLabel = customtkinter.CTkLabel(self, text="Raw CAN Data")
         self.rawCANLabel.grid(row=0, column=0, padx=20, pady=10, sticky="w")
 
+        # Create the scroll to bottom checkbox.
+        self.scrollToBottomCheckbox = customtkinter.CTkCheckBox(self, text="Scroll to Bottom")
+        self.scrollToBottomCheckbox.select()
+        self.scrollToBottomCheckbox.grid(row=0, column=0, padx=20, pady=10, sticky='e')
+
         # Create logging LED
         self.led0 = tk_tools.Led(self, size=40, bg="#333333")
         self.led0.grid(row=0, column=1, padx=10, pady=0)
@@ -29,7 +34,7 @@ class RawCANFrame(customtkinter.CTkFrame):
         self.logCANButton.grid(row=0, column=2, padx=20, pady=10)
 
         # Create the raw CAN viewer.
-        self.canText = customtkinter.CTkTextbox(self)
+        self.canText = customtkinter.CTkTextbox(self, font=("Courier", 12))
         self.canText.grid(row=1, column=0, columnspan=3 , padx=5, pady=(0,5), sticky="nsew")
         # Add the default text
         self.canText.configure(state='normal')
@@ -41,23 +46,25 @@ class RawCANFrame(customtkinter.CTkFrame):
 
     def assign_listener(self):
         # Create a listener
-        listener = RawCANListener(self.canText, self.led0)
+        listener = RawCANListener(self.canText, self.led0, self.scrollToBottomCheckbox)
         self.network.notifier.add_listener(listener)        
         print("Assigned listener to RawCANFrame")
 
 class RawCANListener(Listener):
-    def __init__(self, text:customtkinter.CTkTextbox, led:tk_tools.Led):
+    def __init__(self, text:customtkinter.CTkTextbox, led:tk_tools.Led, scrollToBottom:customtkinter.CTkCheckBox):
         self.text=text
         self.led=led
+        self.scrollToBottom = scrollToBottom
 
     def on_message_received(self, msg:Message) -> None:
         self.led.to_green(True)
         time.sleep(0.02)
-        self.text.configure(state='normal')
-        self.text.insert('end', str(msg)+"\n")
-        self.text.configure(state='disabled')   
-        self.text.see("end") 
-        self.led.to_green(False)
-
-    def on_error(self, exc: Exception) -> None:
-        print("Error in RawCAN listener: " + str(exc))
+        try:
+            self.text.configure(state='normal')
+            self.text.insert('end', str(msg).strip().replace('  ',' ')+"\n")
+            self.text.configure(state='disabled')   
+            if (self.scrollToBottom.get()):
+                self.text.see("end") 
+            self.led.to_green(False)
+        except:
+            print("Error updating can log.")
