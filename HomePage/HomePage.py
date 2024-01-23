34d0ef3e12147b7396f4ce53e7483eaca41d0717
EEvolution-LewisHeslop <lewis.heslop@eevbsl.co.uk>
2024-01-23 10:37:11 +0000

    Updated homepage with aboutbox

--- HomePage/HomePage.py
+++ HomePage/HomePage.py
@@ -12,8 +12,20 @@ class HomeTab(customtkinter.CTkFrame):
         self.grid_rowconfigure(index=0, weight=1)
         self.grid_columnconfigure(index=0, weight=1)
 
-        # # Create the homepage logo.
+        # Create the homepage logo.
         logoFp = "resources/EEvolution_Logo.png"
         logoImage = Image.open(logoFp)
         self.logoStretchy = ResponsiveImage(self, image=logoImage)
-        self.logoStretchy.grid(row=0, column=0, sticky='nsew')
+        self.logoStretchy.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+
+        # Create about box.
+        self.aboutBoxFrame = customtkinter.CTkFrame(self)
+        self.aboutBoxFrame.grid(row=1, column=0, padx=10, pady=10, sticky='nsew')
+        self.aboutBoxFrame.grid_columnconfigure(index=0, weight=1)
+        versionNumber = "0.0.0.1"
+        aboutText = (f"Welcome to EEvoCAN™, an all in one CAN device"
+                     f" configuration, monitoring, and interfacing tool.    "
+                     f" Version {versionNumber}.    "
+                     f" Copyright © 2024 EEvolution Battery Systems.")
+        self.aboutBoxText = customtkinter.CTkLabel(self.aboutBoxFrame, text=aboutText)
+        self.aboutBoxText.grid(row=0, column=0, padx=20, pady=10)
